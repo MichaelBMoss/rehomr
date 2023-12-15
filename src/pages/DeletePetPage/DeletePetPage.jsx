f79abb5e0863938d3236537fa9fac7e7f2cb835c
MichaelBMoss <michaelbmoss@gmail.com>
2023-12-15 14:39:02 -0800

    delete working

--- src/pages/DeletePetPage/DeletePetPage.jsx
+++ src/pages/DeletePetPage/DeletePetPage.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import * as petsAPI from "../../utilities/pets-api";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 
 export default function PetDetailPage() {
 	const [pet, setPet] = useState();
 	const { petId } = useParams();
+  const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchPet = async () => {
@@ -21,8 +24,16 @@ export default function PetDetailPage() {
 
   const handleRemovePet = async () => {
     try {
+      // Make an API request to delete the pet by ID
+      const response = await axios.delete(`/api/pets/${petId}`);
+
+      // Redirect to a different page after successful deletion
+      navigate("/pets");
+
+      console.log("Pet deleted:", response.data);
     } catch (error) {
       console.error(error);
+      // Handle error
     }
   };
 
