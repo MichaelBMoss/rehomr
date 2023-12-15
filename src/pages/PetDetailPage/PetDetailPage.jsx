229c117b1974de80bc5559470cf8055e0bcfd863
Zachary Mitchell <masspike84@yahoo.com>
2023-12-15 11:53:07 -0500

    Try petId but was not connecting

--- src/pages/PetDetailPage/PetDetailPage.jsx
+++ src/pages/PetDetailPage/PetDetailPage.jsx
@@ -3,38 +3,41 @@ import { useParams } from "react-router-dom";
 import * as petsAPI from "../../utilities/pets-api";
 
 export default function PetDetailPage() {
-  const [pet, setPet] = useState();
-  const { id } = useParams();
+	const [pet, setPet] = useState();
+	const { id } = useParams();
 
-  const fetchPet = async () => {
-    try {
-      const data = await petsAPI.getById(id); // Changed from getOnePet to getById
-      setPet(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }; fetchPet();
+  useEffect(() => {
+    const fetchPet = async () => {
+      try {
+        const data = await petsAPI.getById(id);
+        setPet(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-
-
-  return (
-    <>
-      <div>
-        <h1>Pet Details</h1>
-        {pet ?
-          <>
-          <p>Name: {pet.name}</p>
-          <p>Animal Type: {pet.animal}</p>
-            <p>Breed: {pet.breed}</p>
-            <p>Age: {pet.age}</p>
-            <p>Gender: {pet.gender}</p>
-            <p>Bio: {pet.description}</p>
-            {/* <button onClick={handleDeletePet}>Delete Pet</button> */}
-
-          </>
-
-            : <p>Loading...</p>}
-        </div>
-    </>
-  );
+    fetchPet();
+  }, [id]);
+	return (
+		<>
+			<div>
+				<h1>Pet Details</h1>
+				{pet ? (
+					<>
+						<div>
+							<p>Name: {pet.name}</p>
+							<p>Animal Type: {pet.animal}</p>
+							<p>Breed: {pet.breed}</p>
+							<p>Age: {pet.age}</p>
+							<p>Gender: {pet.gender}</p>
+							<p>Bio: {pet.description}</p>
+						</div>
+						{/* <button onClick={handleDeletePet}>Delete Pet</button> */}
+					</>
+				) : (
+					<p>Loading...</p>
+				)}
+			</div>
+		</>
+	);
 }
