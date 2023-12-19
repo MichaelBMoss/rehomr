c7799ea065e84e8c798d96156a7bd27a0958c116
ianmacdonald <ianpetercreative@gmail.com>
2023-12-19 12:23:00 -0700

    start styling org detail page

--- src/pages/OrgsListPage/OrgsListPage.jsx
+++ src/pages/OrgsListPage/OrgsListPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import * as usersAPI from "../../utilities/users-api";
 import { Link } from 'react-router-dom';
+import OrgCard from "../../components/OrgCard/OrgCard";
 
 export default function AllOrgsPage() {
   const [orgs, setOrgs] = useState([]);
@@ -23,16 +24,8 @@ export default function AllOrgsPage() {
         <h1>All Orgs Page</h1>
         <div className="list-group">
           {orgs.map((org) => (
-              <Link className="card org-card" to={`/orgs/${org._id}`} key={org._id}>
-                <div className="org-card-photo">
-                  <img src={`${org.photoUrl}`} alt={`${org.name}`} />
-                </div>
-                <div className="org-card-info">
-                  <h3>{org.name}</h3>
-                  <span>{org.location}</span>
-                </div>
-              </Link>  
-            ))}
+            <OrgCard org={org} key={org._id} />
+          ))}
         </div>
       </div>
     </>
