0d2c5f7625ef6fa0200245cce299ef1fde131537
ianmacdonald <ianpetercreative@gmail.com>
2023-12-19 13:36:18 -0700

    add styling to confirm delete page

--- src/pages/DeletePetPage/DeletePetPage.jsx
+++ src/pages/DeletePetPage/DeletePetPage.jsx
@@ -8,12 +8,12 @@ import axios from "axios";
 export default function DeletePetPage() {
 	const [pet, setPet] = useState();
 	const { petId } = useParams();
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchPet = async () => {
 			try {
-				const data = await dataAPI.getById('/api/pets',petId);
+				const data = await dataAPI.getById('/api/pets', petId);
 				setPet(data);
 			} catch (error) {
 				console.error(error);
@@ -22,31 +22,31 @@ export default function DeletePetPage() {
 		fetchPet();
 	}, [petId]);
 
-  const handleRemovePet = async () => {
-    try {
-      // Make an API request to delete the pet by ID
-      const response = await axios.delete(`/api/pets/${petId}`);
+	const handleRemovePet = async () => {
+		try {
+			// Make an API request to delete the pet by ID
+			const response = await axios.delete(`/api/pets/${petId}`);
 
-      // Redirect to a different page after successful deletion
-      navigate("/pets");
+			// Redirect to a different page after successful deletion
+			navigate("/pets");
 
-      console.log("Pet deleted:", response.data);
-    } catch (error) {
-      console.error(error);
-      // Handle error
-    }
-  };
+			console.log("Pet deleted:", response.data);
+		} catch (error) {
+			console.error(error);
+			// Handle error
+		}
+	};
 
 	return (
 		<>
-			<div>
+			<div className="pet-form">
 				<h1>Delete Pet Page</h1>
 				{pet ? (
 					<>
-						<div>
-							<p>Are you sure you want to remove {pet.name}?</p>
-              <button onClick={handleRemovePet}>Remove</button>
-              <button onClick={() => window.history.back()}>Cancel</button>
+						<p>Are you sure you want to remove {pet.name}?</p>
+						<div className="pet-crud-buttons">
+							<button className="btn btn-red" onClick={handleRemovePet}>Remove</button>
+							<button className="btn btn-yellow-outline" onClick={() => window.history.back()}>Cancel</button>
 						</div>
 					</>
 				) : (
