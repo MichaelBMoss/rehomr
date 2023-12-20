7a29d5f70276fc0c00362f31990d2217fca2113f
MichaelBMoss <michaelbmoss@gmail.com>
2023-12-20 12:09:10 -0800

    minor

--- src/components/PetForm/PetForm.jsx
+++ src/components/PetForm/PetForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import {getUser} from '../../utilities/users-service'
+import { getUser } from '../../utilities/users-service'
 
 
 export default function PetForm({ purpose, formData, setFormData, petId = null }) {
