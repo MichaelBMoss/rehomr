261b374bbac07650c52b012c022cc367b109e680
Zachary Mitchell <masspike84@yahoo.com>
2023-12-20 15:41:45 -0500

    add import for map on allpets2

--- src/pages/AllPetsPage/AllPetsPage.jsx
+++ src/pages/AllPetsPage/AllPetsPage.jsx
@@ -4,6 +4,7 @@ import PetCard from "../../components/PetCard/PetCard";
 import { getDistance } from "geolib";
 import { GoogleMap, MarkerF } from "@react-google-maps/api";
 
+
 export default function AllPetsPage({ user }) {
 	const [pets, setPets] = useState([]);
 	const [sortOrder, setSortOrder] = useState("name");
